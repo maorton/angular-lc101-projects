4b3bcbe8f7c643a769eb54b6c77a451f3510b381
Megan Orton <megan.orton0@gmail.com>
2020-09-13 22:27:42 -0500

    finished lesson 2 exercises

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -24,9 +24,16 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
-
+  addToCrew (newCrewMember: object) {
+    if (!this.crew.includes(newCrewMember)) {
+      this.crew.push(newCrewMember);
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
-
+  changeMissionName (newMissionName: string) {
+    this.missionName = newMissionName;
+    // editMissionName = false;
+  }
 
 }
